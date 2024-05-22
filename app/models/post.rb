b886d47599b04d2e3b8f60dc7d665270ac0916bd
HrishabhTal <135027222+HrishabhTal@users.noreply.github.com>
2024-05-22 17:30:21 +0530

    Add scope to fetch posts by user ID

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :by_user_id, ->(user_id) {
+    joins(:user).where(users: { id: user_id })
+  }
 end
