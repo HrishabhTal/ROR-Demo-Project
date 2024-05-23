f59bf87445922fb88dde859ea51fb6159396784b
HrishabhTal <135027222+HrishabhTal@users.noreply.github.com>
2024-05-23 12:24:26 +0530

    Added custom validation to ensure the password strength

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   has_many :posts
   has_secure_password
  validates:email, presence:true
+
+ private
+
+ def complex_password
+
+   regex = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}\z/
+
+   unless password && regex.match?(password)
+     errors.add(:password, "must contain at least one uppercase letter, one lowercase letter, and one digit.")
+   end
+  end
 end
