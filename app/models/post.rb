07ac90089ef2c8066dafda370bd13db7b2b3d9b0
HrishabhTal <135027222+HrishabhTal@users.noreply.github.com>
2024-05-27 15:32:54 +0530

    Used various metaprogramming define_method,self also callback auto_create to calculate user membership_level everytime user posts something new.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
+  after_create :update_user_membership_level
+
+  private
+
+  def update_user_membership_level
+    user.calculate_membership_level
+  end
+
   scope :by_user_id, ->(user_id) {
     joins(:user).where(users: { id: user_id })
   }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,30 @@
 class User < ApplicationRecord
   has_many :posts
   has_secure_password
- validates:email, presence:true
+  validates:email, presence:true
 
- private
+  define_method("calculate_membership_level") do
+    membership_levels = {
+      "platinum" => 10..Float::INFINITY,
+      "gold" => 5..9,
+      "silver" => 0..4
+    }
 
- def complex_password
+    membership_levels.each do |level, range|
+      if self.posts.count >= range.begin && self.posts.count <= range.end
+        self.update!(membership_level: level)
+        break
+      end
+    end
+  end
+  private
 
-   regex = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}\z/
+  def complex_password
 
-   unless password && regex.match?(password)
+    regex = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}\z/
+
+    unless password && regex.match?(password)
      errors.add(:password, "must contain at least one uppercase letter, one lowercase letter, and one digit.")
+    end
    end
   end
-end
